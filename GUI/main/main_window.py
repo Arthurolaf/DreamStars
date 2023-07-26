8eef1c9938afab46e0d88cc17664b1a3465966f6
Artur.Abaidulov <arturokgot@gmail.com>
2023-07-26 15:48:39 +0600

    Update main_window.py
    Add splitter splitter_msgs_nd_dscr
    Update  load_game now i s self.load_game -- need to passable load new game
    
    Rename splitter var names  --  splitter1 to splitter_ocp_nd_map
    
    Commented the code dev main()

--- GUI/main/main_window.py
+++ GUI/main/main_window.py
@@ -4,42 +4,66 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from Hidenbutton import changeVisibility
 from map import main_map_scen
-
+from widget_panel_part import Window
 import random
 
 class Main_Window(QWidget):
-    def __init__(self):
+    def __init__(self, load_game=None):
         super(Main_Window, self).__init__()
+        self.load_game = load_game
         self.initUI()
-	
+        
+
     def initUI(self):
-        load_game = None
+        
         
         
         hbox = QHBoxLayout(self)
 
-        topleft = QFrame()
-        topleft.setFrameShape(QFrame.StyledPanel)
+        # temporary placeholder
         bottom = QFrame()
         bottom.setFrameShape(QFrame.StyledPanel)
-
-        splitter1 = QSplitter(Qt.Horizontal)
-        textedit = QTextEdit()
-        splitter1.addWidget(topleft)
-
-        if load_game is not None:            
-            model3 = main_map_scen(load_game=load_game)
-        else:
-            model3 = main_map_scen()
-
-
-        splitter1.addWidget(model3.model)    
         
-        splitter1.setSizes([100,200])
+        widget_pl_info = Window("planet_info")
+        corgo_info = Window("corgo_info")
+        # temporary placeholder
+        layout = QVBoxLayout(self)
+        layout.addWidget(corgo_info)
+        layout.addWidget(widget_pl_info)
+        self.control_panel = QWidget()
+        self.control_panel.setLayout(layout)
 
-        splitter2 = QSplitter(Qt.Vertical)
-        splitter2.addWidget(splitter1)
-        splitter2.addWidget(changeVisibility())
+        #bottom.setFrameShape(QFrame.StyledPanel)
+
+        # This area should contain the "Objects control panel" and the Map. Lets add it. 
+        
+        splitter_ocp_nd_map = QSplitter(Qt.Horizontal) # The splitter moves along the horizontal (but the splitter itself is vertical)
+        
+        # Add Objects control panel (control_panel)
+        splitter_ocp_nd_map.addWidget(self.control_panel)
+
+
+        if self.load_game is not None:            
+            map = main_map_scen(load_game=self.load_game)
+        else:
+            map = main_map_scen()
+
+
+        splitter_ocp_nd_map.addWidget(map.model) # Place the map in the splitter area 
+        
+        splitter_ocp_nd_map.setSizes([100,200])
+
+
+        # This area should contain event messages and additional object descriptions.
+        splitter_msgs_nd_dscr = QSplitter(Qt.Horizontal)
+
+
+        splitter_msgs_nd_dscr.addWidget(bottom)
+        splitter_msgs_nd_dscr.addWidget(changeVisibility())
+
+        splitter2 = QSplitter(Qt.Vertical) 
+        splitter2.addWidget(splitter_ocp_nd_map)
+        splitter2.addWidget(splitter_msgs_nd_dscr)
 
         hbox.addWidget(splitter2)
 
@@ -50,10 +74,10 @@ class Main_Window(QWidget):
         self.setWindowTitle('QSplitter demo')
         self.show()
 		
-def main():
-   app = QApplication(sys.argv)
-   ex = Main_Window()
-   sys.exit(app.exec_())
+# def main():
+#    app = QApplication(sys.argv)
+#    ex = Main_Window()
+#    sys.exit(app.exec_())
 	
-if __name__ == '__main__':
-   main()
+# if __name__ == '__main__':
+#    main()
